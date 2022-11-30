46cc1cb907d92cfb6c39872e457cd1f960c91271
tanvhla258 <tanvhla258@gmail.com>
2022-11-30 14:07:42 +0700

    section commit

--- src/Component/Home/HomeSection/HomeSection.js
+++ src/Component/Home/HomeSection/HomeSection.js
@@ -3,14 +3,28 @@ import "./HomeSection.css";
 import Logo from "../../Logo/Logo";
 import Stading from "../../Table/Stading/Stading";
 import MatchSchedule from "../../Table/MatchSchedule/MatchSchedule";
+import Button from "../../Button/Button";
 function HomeSection() {
   return (
     <div className="HomeSection">
       <div className="HomeSectionHeader">
         <Logo value="Premier Leaguage"></Logo>
+      </div>
 
-        <div className="HomeSectionWrapper">
+      <div className="HomeSectionWrapper">
+        <div
+          className="HomeSectionItem HomeSectionNav"
+          style={{ marginLeft: 0 }}
+        >
+          <div className="HomeSectionNavWrapper">
+            <Button>Match List</Button>
+            <Button>Match Result</Button>
+          </div>
+        </div>
+        <div className="HomeSectionItem">
           <Stading></Stading>
+        </div>
+        <div className="HomeSectionItem">
           <MatchSchedule></MatchSchedule>
         </div>
       </div>
