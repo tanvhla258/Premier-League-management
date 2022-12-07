6483d6fc20c9aa04699a926cd8cd2ddaf57f3cb2
tanvhla258 <tanvhla258@gmail.com>
2022-12-07 09:35:01 +0700

    TopScore update

--- src/Component/TopScorePage/TopScorePage.js
+++ src/Component/TopScorePage/TopScorePage.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TopScorePage.css";
 import StandingPageNavBar from "../StandingPage/StadningPageNavBar/StandingPageNavBar";
 import PlayerTable from "../Table/PlayerTable/PlayerTable";
 import Player from "../Table/Player/Player";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faS,
+  faCaretLeft,
+  faCaretRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { TopScoreData } from "../Data/TopScoreData";
 function TopScorePage(props) {
+  let [currentPage, setCurrentPage] = useState(0);
+  const [PlayerPerPage] = useState(3);
+  let maxPage = Math.floor((TopScoreData.length - 1) / PlayerPerPage);
+  console.log(maxPage);
+  let startItem = currentPage * PlayerPerPage;
+  let endItem = startItem + PlayerPerPage;
+
+  let renderPlayerList = TopScoreData?.slice(startItem, endItem);
+
+  function nextClick() {
+    return currentPage < maxPage
+      ? setCurrentPage(currentPage + 1)
+      : currentPage;
+  }
+  function prevClick() {
+    return currentPage > 0 ? setCurrentPage(currentPage - 1) : currentPage;
+  }
   return (
     <div className="TopScorePage">
       <StandingPageNavBar Logo="Top Score" />
@@ -14,37 +37,35 @@ function TopScorePage(props) {
           <span>Point</span>
         </div>
         <div className="TopScoreContent">
-          <div className="TopScoreItem">
-            <div className="TopScoreRank">1</div>
-            <Player
-              type="TopScorePlayer"
-              name="Cristiano Ronaldo"
-              teamName="Chelsea"
-            />
+          <div className="TopScoreContentMain">
+            {renderPlayerList?.map((p) => {
+              return (
+                <div className="TopScoreItem">
+                  <div className="TopScoreRank">{p.rank}</div>
+                  <Player
+                    type="TopScorePlayer"
+                    name={p.name}
+                    teamName="ManUnited"
+                  />
 
-            <div className="TopScorePoint">19</div>
+                  <div className="TopScorePoint">{p.score}</div>
+                </div>
+              );
+            })}
           </div>
-
-          <div className="TopScoreItem">
-            <div className="TopScoreRank">1</div>
-            <Player
-              type="TopScorePlayer"
-              name="Cristiano Ronaldo"
-              teamName="Chelsea"
+          <div className="ControlBtn">
+            <FontAwesomeIcon
+              className="fai"
+              icon={faCaretLeft}
+              size="2x"
+              onClick={prevClick}
             />
-
-            <div className="TopScorePoint">19</div>
-          </div>
-
-          <div className="TopScoreItem">
-            <div className="TopScoreRank">1</div>
-            <Player
-              type="TopScorePlayer"
-              name="Cristiano Ronaldo"
-              teamName="Chelsea"
+            <FontAwesomeIcon
+              className="fai"
+              icon={faCaretRight}
+              size="2x"
+              onClick={nextClick}
             />
-
-            <div className="TopScorePoint">19</div>
           </div>
         </div>
       </div>
