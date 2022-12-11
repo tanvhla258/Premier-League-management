6d3e0d4e93bfe12ce03327376459afdd254b72c7
tanvhla258 <tanvhla258@gmail.com>
2022-12-11 11:24:57 +0700

    add cors npm (require install)

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 
-const colors = require('colors');
-const { readdirSync } = require('fs');
-require('dotenv').config();
+const colors = require("colors");
+const { readdirSync } = require("fs");
+const cors = require("cors");
+require("dotenv").config();
 //const { errorHandler } = require('./middleware/errorMiddleware')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 //routes
 
-
-app.use('/api/users', require('./routes/user.r'));
+app.use("/api/users", require("./routes/user.r"));
 // app.use('/api/goals', require('./routes/goal.r'))
 //app.use('/api / clubs', require('./routes/club.r'))
 // app.use('/api/players', require('./routes/player.r'))
@@ -25,9 +27,7 @@ app.use('/api/users', require('./routes/user.r'));
 // app.use('/api/matches', require('./routes/match.r'))
 // app.use('/api/goals', require('./routes/goal.r'))
 
-
 //app.use(errorHandler)
 app.listen(process.env.PORT, () => {
-    console.log(`http://localhost:${process.env.PORT}`);
-
-})
+  console.log(`http://localhost:${process.env.PORT}`);
+});
