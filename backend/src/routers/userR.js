d3e2428f6669cc34fc96e5ed9fd0f818c63242a9
20120155 <83502703+Foam2702@users.noreply.github.com>
2022-12-12 20:07:37 +0700

    delete wrong src

--- backend/src/routers/userR.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userM = require("../controllers/userC");
-router.get("/login", pageLogin);
-router.post("/login", userLogin);
-router.get("/register", pageRegister);
-router.post("/register", userRegister);
-router.get("/", home);
-module.exports = router;
