2340a16b8e135259fae38f537160900961160a0b
20120155 <83502703+Foam2702@users.noreply.github.com>
2022-12-14 07:56:49 +0700

    fix login

--- frontend/src/Component/LoginPage/LoginForm.js
+++ frontend/src/Component/LoginPage/LoginForm.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import "./LoginForm.css";
-import axios from "axios";
-
 import { Link } from "react-router-dom";
 function LoginForm(props) {
   const [FormType, setFormType] = useState("Login");
@@ -14,21 +12,15 @@ function LoginForm(props) {
     localStorage.setItem("pass", pass.value);
     localStorage.setItem("isLog", 1);
   };
-  const storeFormDataReg = function (data) {
-    // const userReg = document.querySelector(".userReg");
-    // const passReg = document.querySelector(".passReg");
-    // const emailReg = document.querySelector(".emailReg");
+  const storeFormDataReg = function () {
+    const userReg = document.querySelector(".userReg");
+    const passReg = document.querySelector(".passReg");
 
-    // localStorage.setItem("user", userReg.value);
-    // localStorage.setItem("pass", passReg.value);
-    // localStorage.setItem("email", emailReg.value);
-    // localStorage.setItem("isLog", 1);
-    // console.log(emailReg.value);
-    axios.post("http://localhost:3123/api/users", data).then((respone) => {
-      console.log("IT WORKED");
-    });
+    localStorage.setItem("user", userReg.value);
+    localStorage.setItem("pass", passReg.value);
+    localStorage.setItem("isLog", 1);
   };
-  function switchReg() {
+  function swtichReg() {
     setFormType("Register");
   }
   function switchLog() {
@@ -44,10 +36,12 @@ function LoginForm(props) {
           <input className="userReg" type="text" placeholder="name" />
           <input className="passReg" type="password" placeholder="password" />
           <input type="text" placeholder="email address" />
+          <input type="date" id="birthday" name="birthday" />
+          <input className="phoneReg" type="text" placeholder="number phone" />
           <button className="regBtn" onClick={storeFormDataReg}>
             <a className="createacc" href={"/"}>
               Create
-            </a>{" "}
+            </a>
           </button>
           <p onClick={switchLog} className="message">
             Already registered?{" "}
@@ -58,19 +52,19 @@ function LoginForm(props) {
         className="loginBlock"
         style={{ display: FormType == "Login" ? "block" : "none" }}
       >
-        <form className="register-form">
-          <input className="userReg" type="text" placeholder="name" />
-          <input className="passReg" type="password" placeholder="password" />
-          <input type="text" placeholder="email address" />
-          <input type="date" id="birthday" name="birthday" />
-          <input className="phoneReg" type="text" placeholder="number phone" />
-          <button className="regBtn" onClick={storeFormDataReg}>
+        <form className="login-form" onSubmit={props.Homeback}>
+          <input className="userLogin" type="text" placeholder="username" />
+          <input className="passLogin" type="password" placeholder="password" />
+          <button onClick={storeFormData} className="loginBtn">
             <a className="createacc" href={"/"}>
-              Create
+              login
             </a>
           </button>
-          <p onClick={switchLog} className="message">
-            Already registered?{" "}
+          <p className="message">
+            Not registered?{" "}
+            <a onClick={swtichReg} className="createacc" href="#">
+              Create an account
+            </a>
           </p>
         </form>
       </div>
