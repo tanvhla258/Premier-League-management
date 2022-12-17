b72323939c74c53431d373b48090272fdbbe2f4e
20120133 <86962859+fakergenji12@users.noreply.github.com>
2022-12-17 17:21:23 +0700

    them insert tran dau neu khi 2 doi gap nhau lan 2 thi vong dau la vong 2

--- backend/db/truy_van.sql
+++ backend/db/truy_van.sql
@@ -6,6 +6,7 @@ SELECT * FROM ( SELECT 3,'321','gher.gmail.com','VanA','2002-3-24','97584354931'
 WHERE NOT EXISTS (
   select Ten_User from user where Ten_User = 'VanA' and Email = 'gher.gmail.com'
 );
+select * from user;
 	
 
 -- bang xep hang theo thu tu tang dan 
@@ -22,4 +23,15 @@ where Ten_DB = 'Fulham';
 -- tim id,San_Nha doi bong khi co ten doi bong
 select ID_Doi_Bong,San_Nha
 from doi_bong
-where Ten_DB = 'Fulham'
+where Ten_DB = 'Fulham';
+
+
+--
+INSERT tran_dau ( ID_Tran_Dau,DOI_BONG_ID_Doi_Bong_1,DOI_BONG_ID_Doi_Bong_2,Vong_Dau,GIAI_DAU_ID_Giai_Dau,San,Lich_Thi_Dau)
+SELECT * FROM ( SELECT 11, 101, 106, 2, 1, 'Old Trafford','2020-11-4 20:00:00') AS tmp
+WHERE EXISTS (
+  select DOI_BONG_ID_Doi_Bong_1,DOI_BONG_ID_Doi_Bong_2
+  from tran_dau
+  where DOI_BONG_ID_Doi_Bong_1 = 106 and DOI_BONG_ID_Doi_Bong_2 = 101
+);
+select * from tran_dau;
