6dd7e760e0bcfe8f95bcb6e9176a5e3d9f28b3ce
20120155 <83502703+Foam2702@users.noreply.github.com>
2022-12-21 00:22:21 +0700

    add stadium datetime to matchResult

--- backend/controllers/matchResult.c.js
+++ backend/controllers/matchResult.c.js
@@ -2,7 +2,34 @@ const matchResultM = require("../models/matchResult.m");
 
 exports.getAllMatchResult = async (req, res, next) => {
   const matchResult = await matchResultM.getAllMatchResult();
-  res.send(matchResult);
+  var matchParticular = [];
+  for (let i = 0; i < matchResult.length; i++) {
+    var stadium = await matchResultM.getStadiumByHomeID(
+      matchResult[i].TRAN_DAU_DOI_BONG_ID_Doi_Bong_1
+    );
+    var datetime = await matchResultM.getTimeByMatchID(
+      matchResult[i].TRAN_DAU_ID_Tran_Dau
+    );
+    // console.log(stadium);
+    // console.log(datetime);
+
+    matchParticular[i] = {
+      Ten_Doi_Thang: matchResult[i].Ten_Doi_Thang,
+      Ten_Doi_Thua: matchResult[i].Ten_Doi_Thua,
+      TRAN_DAU_ID_Tran_Dau: matchResult[i].TRAN_DAU_ID_Tran_Dau,
+      TRAN_DAU_DOI_BONG_ID_Doi_Bong_1:
+        matchResult[i].TRAN_DAU_DOI_BONG_ID_Doi_Bong_1,
+      TRAN_DAU_DOI_BONG_ID_Doi_Bong_2:
+        matchResult[i].TRAN_DAU_DOI_BONG_ID_Doi_Bong_2,
+      TRAN_DAU_GIAI_DAU_ID_Giai_Dau:
+        matchResult[i].TRAN_DAU_GIAI_DAU_ID_Giai_Dau,
+      Ti_So: matchResult[i].Ti_So,
+      San: stadium[0].San_Nha,
+      Ngay_gio: datetime[0].Lich_Thi_Dau,
+    };
+  }
+
+  res.send(matchParticular);
 };
 
 exports.createMatchResult = async (req, res, next) => {
--- backend/models/matchResult.m.js
+++ backend/models/matchResult.m.js
@@ -1,7 +1,19 @@
-const db = require("../config/db")
-const table_name = "ket_qua_tran_dau"
+const db = require("../config/db");
+const table_name = "ket_qua_tran_dau";
 module.exports = {
-    getAllMatchResult: async () => {
-        return db.load(`select * from ${table_name}`)
-    }
-}
+  getAllMatchResult: async () => {
+    return db.load(`select * from ${table_name}`);
+  },
+  getStadiumByHomeID: async (id) => {
+    const result = await db.load(
+      `select San_Nha from doi_bong where ID_Doi_Bong=${id}`
+    );
+    return result;
+  },
+  getTimeByMatchID: async (id) => {
+    const result = await db.load(
+      `select Lich_Thi_Dau from tran_dau where ID_Tran_Dau=${id}`
+    );
+    return result;
+  },
+};
