72e19af1f97555047dd9a3805cf83caf14694b91
20120155 <83502703+Foam2702@users.noreply.github.com>
2022-12-20 20:04:38 +0700

    respone playername clubname for TopScorePage

--- backend/controllers/topScore.c.js
+++ backend/controllers/topScore.c.js
@@ -1,5 +1,21 @@
 const topScoreM = require("../models/topScore.m");
 
 exports.getAllTopScore = async (req, res, next) => {
-  res.send(await topScoreM.getAllTopScore());
+  const score = await topScoreM.getAllTopScore();
+  var topscore = [];
+  for (let i = 0; i < score.length; i++) {
+    var clubName = await topScoreM.getClubNameByClubID(
+      score[i].CAU_THU_DOI_BONG_ID_Doi_Bong
+    );
+    var playerName = await topScoreM.getPlayerNameByPlayerID(
+      score[i].CAU_THU_ID_Cau_Thu
+    );
+    topscore[i] = {
+      Ten_CT: playerName[0].Ten_CT,
+      Ten_DB: clubName[0].Ten_DB,
+      So_Ban_Thang: score[i].So_Ban_Thang,
+    };
+  }
+  console.log(topscore);
+  res.send(topscore);
 };
