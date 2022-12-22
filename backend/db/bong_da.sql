60b846982d66d9a219dedfdc48d502539ba2e25c
20120155 <83502703+Foam2702@users.noreply.github.com>
2022-12-22 16:31:26 +0700

    add none club to doi_bong

--- backend/db/bong_da.sql
+++ backend/db/bong_da.sql
@@ -211,7 +211,7 @@ ENGINE = InnoDB;
 -- ------------------------------------------------------------------------------------
 
 -- DOI BONG
-INSERT doi_bong (ID_Doi_Bong,Ten_DB,San_nha,Logo) VALUES ('0', '', '',' ');
+INSERT doi_bong (ID_Doi_Bong,Ten_DB,San_nha,Logo) VALUES (0, 'None Club', 'None Stadium',' ');
 INSERT doi_bong (ID_Doi_Bong,Ten_DB,San_nha,Logo) VALUES (101, 'Manchester United', 'Old Trafford',' ');
 INSERT doi_bong (ID_Doi_Bong,Ten_DB,San_nha,Logo) VALUES (102, 'Manchester City', 'Etihad Stadium',' ');
 INSERT doi_bong (ID_Doi_Bong,Ten_DB,San_nha,Logo) VALUES (103, 'Chelsea', 'Stamford Bridge',' ');
